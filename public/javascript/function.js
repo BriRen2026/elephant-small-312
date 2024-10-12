4fbf49ec56a151f860a208c09be257ce15dfd8b6
iivarae <110143594+iivarae@users.noreply.github.com>
2024-10-12 13:41:03 -0400

    Update function.js
    added event for elephant sound onclick

--- public/javascript/function.js
+++ public/javascript/function.js
@@ -1,3 +1,8 @@
 function staticJS() {
     document.getElementById("section-header").innerHTML += "<br/>(this text is a JS placeholder for the static page)";
 }
+
+function elephantSound(){
+	const sound = new Audio("../elephant.mp3");
+	sound.play();
+}
