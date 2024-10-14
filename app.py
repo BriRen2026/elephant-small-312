5b9b214e5a7c18730f2fd0b781ae7d210320061f
iivarae <jennak@twcny.rr.com>
2024-10-14 15:52:29 -0400

    Elephant sound works now

--- app.py
+++ app.py
@@ -5,5 +5,18 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/elephant-maker")
+def elephantMaker():
+    return render_template("elephant-maker.html")
+
+
 if __name__=='__main__':
     app.run(host="localhost",port=8080)
