99edec91582468a357147b574b42580683b6fd00
ponchomint <jdn2@buffalol.edu>
2024-10-27 16:27:14 -0400

    Merged with ZAne!

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -6,39 +6,27 @@ function elephantSound(){
 }
 
 
-function likeElephant(parent){
-	console.log("Pressed Like Button");
-const parent2 = document.getElementById(parent);
-console.log(parent)
-console.log(parent2)
-//Button starts in unlike state.
-//button-like is the non-solid heart. When you click it, it should become the button-unlike so its in a state where you can unlike after liking
-let id = parent2.querySelector("#post_id").value
-console.log(id)
-let like = parent2.querySelector('.button-like');
-let unlike = parent2.querySelector('.button-unlike');
-
-
-//Should increment amount of likes whenever we have that set up...
-let amountOfLikes = parent2.querySelector('#like-counter');
-
-
-let username = document.getElementById('header-user');
-console.log(username.innerText)
-
-
-const data = {"username": username.innerText, "id" : id }; //id : id added by zane, sends the post ID
-
-
-const request = new XMLHttpRequest();
-request.open("POST", "like");
-console.log(data)
-request.send(JSON.stringify(data));
-
-
-like.style.display = "none";
-unlike.style.display = "block";
-
+function likeElephant(parent) {
+	const parent2 = document.getElementById(parent);
+	console.log(parent)
+	console.log(parent2)
+	//Button starts in unlike state.
+	//button-like is the non-solid heart. When you click it, it should become the button-unlike so its in a state where you can unlike after liking
+	let id = parent2.querySelector("#post_id").value
+	console.log(id)
+	let like = parent2.querySelector('.button-like');
+	let unlike = parent2.querySelector('.button-unlike');
+	//Should increment amount of likes whenever we have that set up...
+	let amountOfLikes = parent2.querySelector('#like-counter');
+	let username = document.getElementById('header-user');
+	console.log(username.innerText)
+	const data = {"username": username.innerText, "id": id}; //id : id added by zane, sends the post ID
+	const request = new XMLHttpRequest();
+	request.open("POST", "like");
+	console.log(data)
+	request.send(JSON.stringify(data));
+	like.style.display = "none";
+	unlike.style.display = "block";
 }
 
 function unlikeElephant(parent){
