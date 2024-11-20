ff982a608b54c5afde04a0a928f51fa3612fe8a6
isabel <isabel.k840@gmail.com>
2024-11-20 12:14:26 -0500

    CURRENT FUNCTIONALITY: correct background image, drag/drop events, droppable canvas

--- static/javascript/fashion-game.js
+++ static/javascript/fashion-game.js
@@ -1,17 +1,45 @@
-let backgroundSM;
+let backgroundImg;
+let bckgrndSrc;
 let partyHatImage;
 let partyhat;
 
-//establish images
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text");
+  console.log(data);
+}
+
+function makeCanvasDroppable() {
+    let canvas=document.getElementById("q5Canvas0");
+    canvas.setAttribute("ondrop","drop(event)");
+    canvas.setAttribute("ondragover","allowDrop(event)");
+}
+
+//establish all fashion images
+//TBD: csv file of all fashion item names, script for processing into corresponding vars
 function preload() {
-    //let bckgrndSrc=document.getElementById("")
-    backgroundSM=loadImage("static/images/sleek-mammoth.jpeg");
-    partyHatImage=loadImage("static/images/fashion/partyhat.jpg");
+    partyHatImage=loadImage("static/images/fashion/partyhat2.png");
+}
+
+function setBackground() {
+    let bckgrnd=document.getElementById("elephant-pic").childNodes[0];
+    bckgrndSrc=bckgrnd.src;
+    backgroundImg=loadImage(bckgrndSrc);
+    return backgroundImg;
 }
 
 //build canvas and establish sprites/vectors
 function setup() {
     new Canvas(350,350);
+    makeCanvasDroppable();
     rectMode(CENTER);
     allSprites.rotationLock=true;
 
@@ -22,10 +50,9 @@ function setup() {
 
 }
 
-
 //active game canvas functionality; set background and move sprites
 function draw() {
-    background(backgroundSM);
+    background(setBackground());
 
     if (partyhat.mouse.dragging()) {
         partyhat.moveTowards(mouse.x+partyhat.mouse.x,mouse.y+partyhat.mouse.y,1);
